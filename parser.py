e775294ff636b08390c21cd5b07f724f9eccfcb4
mirrinat <mirzagalamov.rinat1997@mail.ru>
2021-01-19 02:08:46 +0600

    part1(parser) ready

--- parser.py
+++ parser.py
@@ -1,54 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-
+import csv
 
 URL = 'https://alfa.kz/phones/telefony-i-smartfony/page{}#products'
 
-
-phone_dict = {'title': '',
-              'price': '',
-              'seller': '',
-              'ram': '',
-              'memory': ''}
-
 result_phones = []
+
 
 def parse_phone(phone_url):
     response = requests.get(phone_url)
     soup = BeautifulSoup(response.text, "html.parser")
+
+    title = soup.find('h1', {'itemprop': 'name'}).text
+    price = soup.find('span', {'class': 'num'}).text.replace(' ', '')
+    seller = soup.find('div', {'class': 'panel-body'}).find('a').text.strip()
+
     description = soup.find_all('div', {'id': 'additional-info'})
     for feature in description:
         ram = feature.find('dt', {'title': 'Оперативная память'}).find_next().string.strip()
         memory = feature.find('dt', {'title': 'Встроенная память'}).find_next().string.strip()
-    return ram, memory
+    return title, price, seller, ram, memory
 
-for page in range(1):
+
+for page in range(2):
     response = requests.get(URL.format(page))
     soup = BeautifulSoup(response.text, "html.parser")
     phones = soup.find_all("div", {"class": "row product-item product-item-holder"})
     for phone in phones:
         try:
-            title = phone.find('div', {'class': 'title'}).findChildren('h2')[0].text
             phone_url = phone.find('div', {'class': 'title'}).find('a')['href']
         except:
-
             continue
-        price = phone.find('div', {'class': 'price'}).find('span', {'class':'num'}).text
-        seller = phone.find('div', {'class': 'alfa-seller vertical-center'}).find_all('a')[1].text
-        ram, memory = parse_phone(phone_url)
+        title, price, seller, ram, memory = parse_phone(phone_url)
+        phone_dict = {'title': title, 'price': price, 'seller': seller, 'ram': ram, 'memory': memory}
+        result_phones.append(phone_dict)
+        if len(result_phones) == 10:
+            break
 
-        # description = phone.find('div', {'class': 'excerpt'}).find('p', {'itemprop': 'description'}).text.strip()
+keys = result_phones[0].keys()
 
-        # ram = re.search('Оперативная память:(.*),',description)
-        # ram = ram.group(1)
-        # print(phone_url)
-        print(title,price,seller,ram, memory)
-
-
-        # print(phone_url)
-        # print(description)
-
-
-
-# print(phones)
+with open('phones.csv', 'w', newline='') as f:
+    dict_writer = csv.DictWriter(f, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(result_phones)
