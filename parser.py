6d5b9ddefb748a8d90399c45418cd69b3074e851
mirrinat <mirzagalamov.rinat1997@mail.ru>
2021-01-19 01:28:28 +0600

    parser ready

--- parser.py
+++ parser.py
@@ -13,18 +13,42 @@ phone_dict = {'title': '',
               'memory': ''}
 
 result_phones = []
-for i in range(2):
-    response = requests.get(URL.format(i))
+
+def parse_phone(phone_url):
+    response = requests.get(phone_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    description = soup.find_all('div', {'id': 'additional-info'})
+    for feature in description:
+        ram = feature.find('dt', {'title': 'Оперативная память'}).find_next().string.strip()
+        memory = feature.find('dt', {'title': 'Встроенная память'}).find_next().string.strip()
+    return ram, memory
+
+for page in range(1):
+    response = requests.get(URL.format(page))
     soup = BeautifulSoup(response.text, "html.parser")
     phones = soup.find_all("div", {"class": "row product-item product-item-holder"})
-    # print(len(phones))
     for phone in phones:
         try:
-            title = phone.find('div', {'class': 'title'}).findChildren('h2').
+            title = phone.find('div', {'class': 'title'}).findChildren('h2')[0].text
+            phone_url = phone.find('div', {'class': 'title'}).find('a')['href']
         except:
-            print('no')
+
             continue
-        print(title)
+        price = phone.find('div', {'class': 'price'}).find('span', {'class':'num'}).text
+        seller = phone.find('div', {'class': 'alfa-seller vertical-center'}).find_all('a')[1].text
+        ram, memory = parse_phone(phone_url)
+
+        # description = phone.find('div', {'class': 'excerpt'}).find('p', {'itemprop': 'description'}).text.strip()
+
+        # ram = re.search('Оперативная память:(.*),',description)
+        # ram = ram.group(1)
+        # print(phone_url)
+        print(title,price,seller,ram, memory)
+
+
+        # print(phone_url)
+        # print(description)
+
 
 
 # print(phones)
